921aabda54232a98e14cd049639c2e31b7bb2ec6
Victor <vljc17@gmail.com>
2020-12-13 15:53:49 +0800

    opt: preload users for completed and current loans associations

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -21,7 +21,7 @@ module Api
       header 'Content-Type', 'application/json'
       header 'Accept', 'application/json'
       def books
-        @books = Book.includes(:current_loans).includes(:completed_loans).includes(loans: :user)
+        @books = Book.includes(:current_loans).includes(current_loans: :user).includes(:completed_loans).includes(completed_loans: :user).includes(loans: :user)
       end
 
       api :POST, '/create_user', "Create a user interface, the requested parameters support setting the initial amount, returning the user ID"
